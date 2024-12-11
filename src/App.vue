ed071f43faf9a139fd6cd2dced634737ccf9996b
Md Mahfoozur Rahman <mahfoozur.rahman.bd@gmail.com>
2024-12-12 03:15:44 +0600

    task listing html

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
     <div class="container mx-auto p-6 bg-white shadow-lg rounded-lg max-w-5xl">
       <div class="flex justify-between items-center mb-5">
         <h2 class="text-2xl font-bold text-gray-800">All Tasks</h2>
-        <button class="bg-blue-500 text-white px-4 py-2 font-semibold text-sm rounded hover:bg-blue-600">Add Task</button>
+        <button class="bg-blue-500 text-white px-4 py-2 font-semibold text-sm rounded hover:bg-blue-600">Task Form</button>
       </div>
 
       <!-- Add Task Form -->
@@ -20,6 +20,22 @@
         <button class="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600" > Add Task </button>
       </div>
 
+      <!-- Task List -->
+      <div class="mt-6">
+        <p class="text-center text-gray-500 border rounded p-4">
+          No tasks available
+        </p>
+
+        <ul>
+          <li class="p-4 mb-2 border rounded flex justify-between items-center">
+            <div>
+              <input type="checkbox" class="mr-2" />
+              <span>Task 1</span>
+            </div>
+            <button class="text-red-500 hover:text-red-700">Delete</button>
+          </li>
+        </ul>
+      </div>
     </div>
   </main>
 
